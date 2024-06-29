240278670efc8d4132a7c29fdcdda3efea5822ee
unknown <38992201+richgrov@users.noreply.github.com>
2024-06-29 16:05:49 -0600

    Move app logic into example

--- example/main.cc
+++ example/main.cc
@@ -1,14 +1,24 @@
 #include <exception>
 #include <iostream>
 
-#include "app.h"
+#include "gpu/gpu.h"
+#include "window/window.h" // IWYU pragma: export
 
 using namespace villa;
 
 int main(int argc, char **argv) {
    try {
-      App app;
-      app.run();
+      Window window("villa");
+      Gpu gpu;
+
+      gpu.init(window.vulkan_extensions());
+
+      auto surface = window.create_surface(gpu.instance());
+      gpu.connect_to_surface(surface, window.width(), window.height());
+
+      while (window.poll()) {
+         gpu.draw();
+      }
    } catch (const std::exception &e) {
       std::cerr << "Unhandled exception: " << e.what() << "\n";
    }
