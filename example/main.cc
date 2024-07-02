0a5f5f78cc115cc4bf66d5a07efa268d3caac49a
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-02 10:23:16 -0600

    Add drawing application

--- example/main.cc
+++ example/main.cc
@@ -28,20 +28,36 @@ int main(int argc, char **argv) {
       gpu.init(window.vulkan_extensions());
 
       auto surface = window.create_surface(gpu.instance());
-      gpu.connect_to_surface(surface, window.width(), window.height());
+      int width = window.width();
+      int height = window.height();
+      gpu.connect_to_surface(surface, width, height);
 
       auto pipeline = gpu.create_pipeline<Vertex>();
 
-      Vertex vertices[] = {
+      std::vector<Vertex> vertices{
           {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
           {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
           {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
       };
-
-      auto vertex_buffer = gpu.allocate_vertex_buffer<Vertex>(3);
-      vertex_buffer.upload_memory(&vertices, sizeof(vertices));
+      auto vertex_buffer = gpu.allocate_vertex_buffer<Vertex>(vertices.size());
+      vertex_buffer.upload_memory(vertices.data(), vertices.size() * sizeof(Vertex));
 
       while (window.poll()) {
+         if (window.left_clicking()) {
+            float mouse_x = static_cast<float>(window.mouse_x());
+            float mouse_y = static_cast<float>(window.mouse_y());
+
+            float norm_x = mouse_x / width * 2.f - 1.f;
+            float norm_y = mouse_y / height * 2.f - 1.f;
+
+            vertices.push_back({{norm_x, norm_y - .01f}, {1.0, 1.0, 1.0}});
+            vertices.push_back({{norm_x + .01f, norm_y + .01f}, {1.0, 1.0, 1.0}});
+            vertices.push_back({{norm_x - .01f, norm_y + .01f}, {1.0, 1.0, 1.0}});
+
+            vertex_buffer = std::move(gpu.allocate_vertex_buffer<Vertex>(vertices.size()));
+            vertex_buffer.upload_memory(vertices.data(), vertices.size() * sizeof(Vertex));
+         }
+
          gpu.draw(pipeline, vertex_buffer);
          gpu.wait_idle();
       }
