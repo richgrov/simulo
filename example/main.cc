5b2209fde35d80591111a9c5fb3b501f1cf6be8d
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-05 22:48:52 -0600

    Add fireworks example

--- example/main.cc
+++ example/main.cc
@@ -1,6 +1,9 @@
 #include <array>
+#include <cmath>
 #include <exception>
 #include <iostream>
+#include <numbers>
+#include <vector>
 
 #include "game.h"
 #include "gpu/buffer.h"
@@ -14,12 +17,47 @@ using namespace villa;
 
 struct Vertex {
    Vec2 pos;
-   Vec3 color;
 
-   static constexpr std::array<VertexAttribute, 2> attributes{
+   static constexpr std::array<VertexAttribute, 1> attributes{
        VertexAttribute::vec2(),
-       VertexAttribute::vec3(),
    };
+};
+
+struct ParticleUniform {
+   Vec2 pos;
+   Vec3 color;
+};
+
+class Particle {
+public:
+   Particle(float x, float y, Vec3 color)
+       : uniform_({.pos{x, y}, .color = color}), lifespan_(randf() * 4) {
+      float angle = randf() * std::numbers::pi_v<float> * 2;
+      float magnitude = randf();
+      velocity_ = {
+          std::cosf(angle) * magnitude,
+          std::sinf(angle) * magnitude,
+      };
+   }
+
+   void update(float delta) {
+      velocity_.y += 0.5 * delta;
+      uniform_.pos += velocity_ * delta;
+      lifespan_ -= 0.01;
+
+      if (lifespan_ < 0.05) {
+         uniform_.color = {1.0, 1.0, 1.0};
+      }
+   }
+
+   float lifespan() const {
+      return lifespan_;
+   }
+
+private:
+   ParticleUniform uniform_;
+   Vec2 velocity_;
+   float lifespan_;
 };
 
 int main(int argc, char **argv) {
@@ -29,61 +67,55 @@ int main(int argc, char **argv) {
       auto pipeline = game.create_pipeline<Vertex>();
       auto staging_buffer = game.create_staging_buffer(1024);
 
-      auto uniform_buffer = game.create_uniform_buffer<Vec2>(2);
+      auto uniform_buffer = game.create_uniform_buffer<ParticleUniform>(512);
       auto descriptor_pool = game.create_descriptor_pool(pipeline);
       auto descriptor_set = descriptor_pool.allocate(uniform_buffer);
 
-      Vec2 offset = {0.1, 0.1};
-      uniform_buffer.upload_memory(&offset, sizeof(Vec2), 0);
-      offset = {-0.1, -0.1};
-      uniform_buffer.upload_memory(&offset, sizeof(Vec2), 1);
-
       Vertex vertices[] = {
-          {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-          {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-          {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+          {{-0.01f, -0.01f}},
+          {{0.01f, -0.01f}},
+          {{0.01f, 0.01f}},
+          {{-0.01f, 0.01f}},
       };
-      VertexIndexBuffer::IndexType indices[] = {0, 1, 2};
+      VertexIndexBuffer::IndexType indices[] = {0, 1, 2, 0, 2, 3};
 
-      auto mesh_buffer = game.create_vertex_index_buffer<Vertex>(3, 3);
+      std::vector<Particle> particles;
+
+      auto mesh_buffer = game.create_vertex_index_buffer<Vertex>(4, 6);
       staging_buffer.upload_mesh(vertices, sizeof(vertices), indices, VILLA_ARRAY_LEN(indices));
       game.buffer_copy(staging_buffer, mesh_buffer);
 
       while (game.poll()) {
-         /*if (game.left_clicking()) {
+         float delta = game.delta();
+
+         if (game.left_clicked_now()) {
             float mouse_x = static_cast<float>(game.mouse_x());
             float mouse_y = static_cast<float>(game.mouse_y());
 
             float norm_x = mouse_x / game.width() * 2.f - 1.f;
             float norm_y = mouse_y / game.height() * 2.f - 1.f;
 
-            vertices.push_back({{norm_x, norm_y - .01f}, {randf(), randf(), randf()}});
-            vertices.push_back({{norm_x + .01f, norm_y + .01f}, {randf(), randf(), randf()}});
-            vertices.push_back({{norm_x - .01f, norm_y + .01f}, {randf(), randf(), randf()}});
+            Vec3 color = {randf(), randf(), randf()};
+            for (int i = 0; i < 92; ++i) {
+               particles.emplace_back(Particle(norm_x, norm_y, color));
+            }
+         }
 
-            IndexBuffer::IndexType index = indices.size();
-            indices.push_back(index);
-            indices.push_back(index + 1);
-            indices.push_back(index + 2);
+         std::erase_if(particles, [](const Particle &particle) {
+            return particle.lifespan() <= 0.0;
+         });
 
-            staging_buffer.upload_memory(vertices.data(), sizeof(Vertex) * vertices.size());
-            auto new_vertex_buf = game.create_vertex_buffer<Vertex>(vertices.size());
-            game.buffer_copy(staging_buffer, new_vertex_buf);
-            vertex_buffer = std::move(new_vertex_buf);
-
-            staging_buffer.upload_memory(
-                indices.data(), sizeof(IndexBuffer::IndexType) * indices.size()
-            );
-            auto new_index_buf = game.create_index_buffer(indices.size());
-            game.buffer_copy(staging_buffer, new_index_buf);
-            index_buffer = std::move(new_index_buf);
-         }*/
+         for (int i = 0; i < particles.size(); ++i) {
+            Particle &particle = particles[i];
+            particle.update(delta);
+            uniform_buffer.upload_memory(&particles[i], sizeof(ParticleUniform), i);
+         }
 
          if (game.begin_draw(pipeline)) {
-            game.set_uniform(pipeline, descriptor_set, 0);
-            game.draw(mesh_buffer);
-            game.set_uniform(pipeline, descriptor_set, uniform_buffer.element_size());
-            game.draw(mesh_buffer);
+            for (int i = 0; i < particles.size(); ++i) {
+               game.set_uniform(pipeline, descriptor_set, i * uniform_buffer.element_size());
+               game.draw(mesh_buffer);
+            }
             game.end_draw();
          }
 
--- src/shader/shader.vert
+++ src/shader/shader.vert
@@ -1,13 +1,15 @@
 #version 450
 
 layout(location = 0) in vec2 pos;
-layout(location = 1) in vec3 color;
 
-layout(binding = 0) uniform ModelViewProjection { vec2 offset; } u_mvp;
+layout(binding = 0) uniform ModelViewProjection {
+    vec2 pos;
+    vec3 color;
+} u;
 
 layout(location = 0) out vec3 pass_color;
 
 void main() {
-    gl_Position = vec4(u_mvp.offset + pos, 0.0, 1.0);
-    pass_color = color;
+    gl_Position = vec4(pos + u.pos, 0.0, 1.0);
+    pass_color = u.color;
 }
