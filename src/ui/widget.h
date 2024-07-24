d7dce52ce4e329cd60dcf9e6a615e9ff06162c21
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-24 15:02:49 -0600

    Make Widget functions inline

--- src/ui/widget.h
+++ src/ui/widget.h
@@ -11,16 +11,16 @@ public:
    Widget(std::vector<UiVertex> &&vertices, std::vector<VertexIndexBuffer::IndexType> &&indices)
        : Mesh(std::move(vertices), std::move(indices)) {}
 
-   void set_position(int x, int y) {
+   inline void set_position(int x, int y) {
       x_ = x;
       y_ = y;
    }
 
-   void set_size(int size) {
+   inline void set_size(int size) {
       scale_ = size;
    }
 
-   Mat4 model_matrix() const {
+   inline Mat4 model_matrix() const {
       return Mat4::translate(Vec3(x_, y_, 0)) * Mat4::scale(Vec3(scale_, scale_, 1));
    }
 
