34630fb0f9515c5f6b421f9a09ed29757b1a2429
unknown <38992201+richgrov@users.noreply.github.com>
2024-07-24 15:32:55 -0600

    Refactor font vertex construction

--- src/ui/font.cc
+++ src/ui/font.cc
@@ -2,6 +2,7 @@
 #include "gpu/buffer.h"
 #include "gpu/image.h"
 #include "gpu/physical_device.h"
+#include "math/vec2.h"
 #include "ui/ui.h"
 #include "ui/widget.h"
 #include "vulkan/vulkan_core.h"
@@ -54,11 +55,14 @@ Widget Font::create_text(const std::string &text) {
       stbtt_aligned_quad q;
       stbtt_GetBakedQuad(chars_.data(), BITMAP_WIDTH, BITMAP_WIDTH, index, &x, &y, &q, 1);
 
+      Vec2 pos1 = Vec2(q.x0 + x_off, -q.y0) / 32.0f;
+      Vec2 pos2 = Vec2(q.x1 + x_off, -q.y1) / 32.0f;
+
       int ind = vertices.size();
-      vertices.push_back({{(q.x0 + x_off) / 32, -q.y0 / 32, 0}, {q.s0, q.t0}});
-      vertices.push_back({{(q.x1 + x_off) / 32, -q.y0 / 32, 0}, {q.s1, q.t0}});
-      vertices.push_back({{(q.x1 + x_off) / 32, -q.y1 / 32, 0}, {q.s1, q.t1}});
-      vertices.push_back({{(q.x0 + x_off) / 32, -q.y1 / 32, 0}, {q.s0, q.t1}});
+      vertices.push_back({{pos1.x, pos1.y, 0}, {q.s0, q.t0}});
+      vertices.push_back({{pos2.x, pos1.y, 0}, {q.s1, q.t0}});
+      vertices.push_back({{pos2.x, pos2.y, 0}, {q.s1, q.t1}});
+      vertices.push_back({{pos1.x, pos2.y, 0}, {q.s0, q.t1}});
       indices.push_back(ind);
       indices.push_back(ind + 1);
       indices.push_back(ind + 2);
