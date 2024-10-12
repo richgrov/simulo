0d777bc1b96bf7a1f04f35066af293f308a9c684
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-10-11 22:50:22 -0600

    Add dummy key defs for linux

---
+++ src/window/x11/keys.h
@@ -0,0 +1,15 @@
+#ifndef VKAD_WINDOW_X11_KEYS_H_
+#define VKAD_WINDOW_X11_KEYS_H_
+
+#define VKAD_KEY_ESC 0
+#define VKAD_KEY_A 0
+#define VKAD_KEY_D 0
+#define VKAD_KEY_E 0
+#define VKAD_KEY_C 0
+#define VKAD_KEY_P 0
+#define VKAD_KEY_S 0
+#define VKAD_KEY_SPACE 0
+#define VKAD_KEY_SHIFT 0
+#define VKAD_KEY_W 0
+
+#endif // !VKAD_WINDOW_X11_KEYS_H_
