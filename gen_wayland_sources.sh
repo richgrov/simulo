a8136dc692769801d6367c8bb040efd84eeb38fe
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-11-20 13:01:35 -0700

    Add files for zwp_relative_pointer_v1 protocol

--- gen_wayland_sources.sh
+++ gen_wayland_sources.sh
@@ -1,2 +1,5 @@
 wayland-scanner client-header /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml src/window/linux/xdg-shell-client-protocol.h
 wayland-scanner private-code /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml src/window/linux/xdg-shell-protocol.c
+
+wayland-scanner client-header /usr/share/wayland-protocols/unstable/relative-pointer/relative-pointer-unstable-v1.xml src/window/linux/zwp-relative-pointer-v1-client-protocol.h
+wayland-scanner private-code /usr/share/wayland-protocols/unstable/relative-pointer/relative-pointer-unstable-v1.xml src/window/linux/zwp-relative-pointer-v1-protocol.c
