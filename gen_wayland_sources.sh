7553328be6dd917b0761cdc6c55e5b033caf3aa8
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-11-19 21:27:01 -0700

    Add script to generate wayland source files

---
+++ gen_wayland_sources.sh
@@ -0,0 +1,2 @@
+wayland-scanner client-header /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml src/window/linux/xdg-shell-client-protocol.h
+wayland-scanner private-code /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml src/window/linux/xdg-shell-protocol.c
