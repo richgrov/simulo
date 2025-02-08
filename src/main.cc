203b76cded6d61c59c65c8c893ea62731f505383
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-07 20:56:42 -0700

    feat: Add PNG parsing functionality with directory iteration

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,19 @@
 
 #include "app.h"
 
+#include "image/png.h"
+
 using namespace vkad;
 
 int main(int argc, char **argv) {
+   std::string dir("/home/richard/Documents/FantasyEngine/sprites");
+   // iterate all files in this directory and run this function for the data of each. AI!
+   vkad::parse_png(std::span<const uint8_t> data)
+
+       if (true) {
+      return 0;
+   }
+
    try {
       App app;
 
