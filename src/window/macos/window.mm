4be188d343490b629b66407d42d23578df8a2beb
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-15 21:15:57 -0700

    Render to metal layer

--- src/window/macos/window.mm
+++ src/window/macos/window.mm
@@ -1,9 +1,40 @@
 #import <AppKit/AppKit.h>
 #import <Cocoa/Cocoa.h>
+#import <Metal/Metal.h>
+#import <QuartzCore/CAMetalLayer.h>
+#import <objc/objc.h>
 
 #include "window.h"
 
 using namespace vkad;
+
+namespace {
+
+void resize_metal_layer_to_window(NSWindow *window, CAMetalLayer *metal_layer) {
+   metal_layer.drawableSize =
+       [window.contentView convertSizeToBacking:window.contentView.frame.size];
+}
+
+} // namespace
+
+@interface WindowDelegate : NSObject <NSWindowDelegate> {
+   BOOL _closed;
+}
+@end
+
+@implementation WindowDelegate
+
+- (void)windowDidResize:(NSNotification *)notification {
+   NSWindow *window = notification.object;
+   CAMetalLayer *metal_layer = (CAMetalLayer *)window.contentView.layer;
+   resize_metal_layer_to_window(window, metal_layer);
+}
+
+- (void)windowWillClose:(NSNotification *)notification {
+   _closed = true;
+}
+
+@end
 
 Window::Window(const char *title) {
    [NSApplication sharedApplication];
@@ -15,9 +46,19 @@ Window::Window(const char *title) {
                    backing:NSBackingStoreBuffered
                      defer:NO];
 
+   window.delegate = [[WindowDelegate alloc] init];
+
    [window center];
-   [window setTitle:[NSString stringWithUTF8String:title]];
+   window.title = [NSString stringWithUTF8String:title];
+   window.isVisible = YES;
    [window makeKeyAndOrderFront:nil];
+   window.releasedWhenClosed = NO;
+
+   CAMetalLayer *metal_layer = [[CAMetalLayer alloc] init];
+   metal_layer.device = MTLCreateSystemDefaultDevice();
+   metal_layer.opaque = YES;
+   resize_metal_layer_to_window(window, metal_layer);
+   window.contentView.layer = metal_layer;
 
    this->ns_window_ = window;
 }
