c04e42382fab9c6a1550ebadf36844b0d269f134
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-18 08:38:36 -0700

    Print fatal exceptions

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,20 @@
+#include <exception>
+#include <iostream>
+
 #include "app.h"
 
 using namespace vkad;
 
 int main(int argc, char **argv) {
-   App app;
+   try {
+      App app;
 
-   while (app.poll()) {
-      app.draw();
-      app.renderer().wait_idle();
+      while (app.poll()) {
+         app.draw();
+         app.renderer().wait_idle();
+      }
+   } catch (const std::exception &ex) {
+      std::cerr << "unhandled exception: " << ex.what() << "\n";
    }
 
    return 0;
