2a369c5f86f924223f6df19f967775a8b10c4435
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-03-18 20:50:02 -0600

    Fix commit error

--- src/gpu/metal/buffer.mm
+++ src/gpu/metal/buffer.mm
@@ -9,9 +9,9 @@
 using namespace vkad;
 
 Buffer::Buffer(const Gpu &gpu, std::span<const uint8_t> data) {
-   buffer_ = [device_ newBufferWithBytes:data.data()
-                                  length:data.size()
-                                 options:MTLResourceStorageModeShared];
+   buffer_ = [gpu.device() newBufferWithBytes:data.data()
+                                       length:data.size()
+                                      options:MTLResourceStorageModeShared];
    if (buffer_ == nullptr) {
       throw std::runtime_error("failed to create metal buffer");
    }
