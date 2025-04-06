e9101c2d5b3c524e9363d63be46390ca0f2897da
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-04-06 14:39:45 -0600

    First gdscript perception api

--- perception_sdk/godot/register_types.cc
+++ perception_sdk/godot/register_types.cc
@@ -1,21 +1,69 @@
 #include <gdextension_interface.h>
+#include <godot_cpp/classes/ref.hpp>
 #include <godot_cpp/classes/ref_counted.hpp>
+#include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
+#include <godot_cpp/variant/array.hpp>
+
+#include "../perception.h"
 
 using namespace godot;
 
 namespace godot {
 
+class Detection : public RefCounted {
+   GDCLASS(Detection, RefCounted);
+
+public:
+   Detection() {}
+
+   Vector2 get_keypoint(int keypoint_index) {
+      simulo::Perception::Keypoint kp = detection_.points[keypoint_index];
+      return Vector2(kp.x, kp.y);
+   }
+
+   simulo::Perception::Detection detection_;
+
+protected:
+   static void _bind_methods() {
+      ClassDB::bind_method(D_METHOD("get_keypoint", "keypoint_index"), &Detection::get_keypoint);
+   }
+};
+
 class Perception2d : public RefCounted {
    GDCLASS(Perception2d, RefCounted);
 
 public:
-   Perception2d() {}
-   ~Perception2d() {}
+   Perception2d() {
+      perception_.set_running(true);
+   }
+
+   ~Perception2d() {
+      perception_.set_running(false);
+   }
+
+   Array detect() {
+      std::vector<simulo::Perception::Detection> detections = perception_.latest_detections();
+
+      Array result;
+      for (auto &&detection : detections) {
+         Ref<Detection> det;
+         det.instantiate();
+         det->detection_ = std::move(detection);
+         result.push_back(det);
+      }
+
+      return result;
+   }
 
 protected:
-   static void _bind_methods() {}
+   static void _bind_methods() {
+      ClassDB::bind_method(D_METHOD("detect"), &Perception2d::detect);
+   }
+
+private:
+   simulo::Perception perception_;
 };
 
 } // namespace godot
@@ -25,6 +73,7 @@ void init_perception(ModuleInitializationLevel level) {
       return;
    }
 
+   GDREGISTER_RUNTIME_CLASS(Detection);
    GDREGISTER_RUNTIME_CLASS(Perception2d);
 }
 
