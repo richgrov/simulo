effa0978bfce5deaed7dfbc666929da57a44f090
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-06-03 19:21:24 -0600

    Add FixedArrayList

---
+++ src/util/fixed_arraylist.zig
@@ -0,0 +1,23 @@
+pub fn FixedArrayList(comptime T: type, comptime capacity: usize) type {
+    return struct {
+        data: [capacity]T = undefined,
+        len: usize = 0,
+
+        const Self = @This();
+
+        pub fn init() Self {
+            return .{};
+        }
+
+        pub fn append(self: *Self, item: T) !void {
+            if (self.len == capacity) return error.OutOfMemory;
+            self.data[self.len] = item;
+            self.len += 1;
+        }
+
+        pub fn get(self: Self, index: usize) ?T {
+            if (index >= self.len) return null;
+            return self.data[index];
+        }
+    };
+}
