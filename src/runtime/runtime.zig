e79a4a6ecd6ee10eac3af1ad745d383338d3dbae
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-06-05 21:35:43 -0600

    Refactor runtime into struct

--- src/runtime/runtime.zig
+++ src/runtime/runtime.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 const engine = @import("engine");
 const Mat4 = engine.math.Mat4;
+const FixedArrayList = @import("../util/fixed_arraylist.zig").FixedArrayList;
 
 comptime {
     _ = engine;
@@ -10,6 +11,112 @@ comptime {
 const Vertex = struct {
     position: @Vector(3, f32) align(16),
     tex_coord: @Vector(2, f32) align(8),
+};
+
+const vertices = [_]Vertex{
+    .{ .position = .{ 0.0, 0.0, 0.0 }, .tex_coord = .{ 0.0, 0.0 } },
+    .{ .position = .{ 1.0, 0.0, 0.0 }, .tex_coord = .{ 1.0, 0.0 } },
+    .{ .position = .{ 1.0, 1.0, 0.0 }, .tex_coord = .{ 1.0, 1.0 } },
+    .{ .position = .{ 0.0, 1.0, 0.0 }, .tex_coord = .{ 0.0, 1.0 } },
+};
+
+const Runtime = struct {
+    gpu: engine.Gpu,
+    window: engine.Window,
+    renderer: engine.Renderer,
+    event_handlers: FixedArrayList(engine.Scripting.Function, 16),
+    scripting: engine.Scripting,
+    pose_detector: engine.PoseDetector,
+    allocator: std.mem.Allocator,
+
+    material: engine.Renderer.MaterialHandle,
+    mesh: engine.Renderer.MeshHandle,
+    chessboard: engine.Renderer.ObjectHandle,
+    tracked_objects: std.AutoHashMap(u64, engine.Renderer.ObjectHandle),
+    calibrated: bool,
+
+    fn init(runtime: *Runtime, allocator: std.mem.Allocator) void {
+        runtime.allocator = allocator;
+
+        runtime.gpu = engine.Gpu.init();
+        runtime.window = engine.Window.init(&runtime.gpu, "simulo runtime");
+        runtime.renderer = engine.Renderer.init(&runtime.gpu, &runtime.window);
+        runtime.event_handlers = FixedArrayList(engine.Scripting.Function, 16).init();
+        runtime.scripting = engine.Scripting.init(runtime);
+        runtime.pose_detector = engine.PoseDetector.init();
+        runtime.tracked_objects = std.AutoHashMap(u64, engine.Renderer.ObjectHandle).init(allocator);
+        runtime.calibrated = false;
+
+        const module = runtime.scripting.defineModule("simulo");
+        const func = runtime.scripting.createFunction(Runtime.registerEventHandler);
+        runtime.scripting.defineFunction(module, "on", func);
+
+        const image = createChessboard(&runtime.renderer);
+        runtime.material = runtime.renderer.createUiMaterial(image, 1.0, 1.0, 1.0);
+        runtime.mesh = runtime.renderer.createMesh(std.mem.asBytes(&vertices), &[_]u16{ 0, 1, 2, 2, 3, 0 });
+        runtime.chessboard = runtime.renderer.addObject(runtime.mesh, Mat4.identity(), runtime.material);
+    }
+
+    fn deinit(self: *Runtime) void {
+        self.tracked_objects.deinit();
+        self.pose_detector.stop();
+        self.scripting.deinit();
+        self.renderer.deinit();
+        self.window.deinit();
+        self.gpu.deinit();
+    }
+
+    fn runScript(self: *Runtime, source: []const u8, file_name: []const u8) !void {
+        try self.scripting.run(source, file_name);
+    }
+
+    fn registerEventHandler(user_ptr: *anyopaque, callback: engine.Scripting.Function) void {
+        var runtime: *Runtime = @alignCast(@ptrCast(user_ptr));
+        runtime.event_handlers.append(callback) catch unreachable;
+    }
+
+    fn run(self: *Runtime) !void {
+        try self.pose_detector.start();
+
+        while (self.window.poll()) {
+            const width: f32 = @floatFromInt(self.window.getWidth());
+            const height: f32 = @floatFromInt(self.window.getHeight());
+
+            if (self.calibrated) {
+                self.renderer.setObjectTransform(self.chessboard, Mat4.scale(.{ 0, 0, 0 }));
+            } else {
+                self.renderer.setObjectTransform(self.chessboard, Mat4.scale(.{ width, height, 1 }));
+            }
+
+            const ui_projection = Mat4.ortho(width, height, -1.0, 1.0);
+            _ = self.renderer.render(&ui_projection, &ui_projection);
+
+            try self.processPoseDetections(width, height);
+        }
+    }
+
+    fn processPoseDetections(self: *Runtime, width: f32, height: f32) !void {
+        while (self.pose_detector.nextEvent()) |event| {
+            const detection = event.detection orelse {
+                const object = self.tracked_objects.get(event.id).?;
+                self.renderer.deleteObject(object);
+                if (!self.tracked_objects.remove(event.id)) {
+                    std.log.err("tracked object wasn't deleted", .{});
+                }
+                continue;
+            };
+
+            const left_hand = detection.keypoints[9].pos;
+            const translate = Mat4.translate(.{ left_hand[0] * width - 25.0, left_hand[1] * height - 25.0, 0 });
+            const scale = Mat4.scale(.{ 50, 50, 1 });
+            const transform = translate.matmul(&scale);
+
+            const object = self.renderer.addObject(self.mesh, transform, self.material);
+            try self.tracked_objects.put(event.id, object);
+
+            self.calibrated = true;
+        }
+    }
 };
 
 pub fn createChessboard(renderer: *engine.Renderer) engine.Renderer.ImageHandle {
@@ -28,11 +135,6 @@ pub fn createChessboard(renderer: *engine.Renderer) engine.Renderer.ImageHandle 
     return renderer.createImage(&checkerboard, 1280, 800);
 }
 
-fn registerHandler(user_ptr: *anyopaque, callback: engine.Scripting.Function) void {
-    var event_handlers: *std.ArrayList(engine.Scripting.Function) = @alignCast(@ptrCast(user_ptr));
-    event_handlers.append(callback) catch unreachable;
-}
-
 pub fn main() !void {
     var dba = std.heap.DebugAllocator(.{}).init;
     defer {
@@ -42,17 +144,12 @@ pub fn main() !void {
     }
     const allocator = dba.allocator();
 
-    var event_handlers = std.ArrayList(engine.Scripting.Function).init(allocator);
-    defer event_handlers.deinit();
-
-    const scripting = engine.Scripting.init(&event_handlers);
-    defer scripting.deinit();
-    const module = scripting.defineModule("simulo");
-    const func = scripting.createFunction(registerHandler);
-    scripting.defineFunction(module, "test", func);
+    var runtime: Runtime = undefined;
+    Runtime.init(&runtime, allocator);
+    defer runtime.deinit();
 
     var args = try std.process.argsWithAllocator(allocator);
-    args.deinit();
+    defer args.deinit();
 
     _ = args.next(); // skip program name
     const script_path = args.next() orelse {
@@ -66,72 +163,6 @@ pub fn main() !void {
     };
     defer allocator.free(script_file);
 
-    try scripting.run(script_file, script_path);
-
-    for (event_handlers.items) |handler| {
-        scripting.callFunction(&handler);
-    }
-
-    const gpu = engine.Gpu.init();
-    defer gpu.deinit();
-
-    var window = engine.Window.init(&gpu, "Simulo");
-    defer window.deinit();
-
-    var renderer = engine.Renderer.init(&gpu, &window);
-    defer renderer.deinit();
-
-    var pose_detector = engine.PoseDetector.init();
-    defer pose_detector.stop();
-    pose_detector.start() catch unreachable;
-
-    const image = createChessboard(&renderer);
-    const material = renderer.createUiMaterial(image, 1.0, 1.0, 1.0);
-
-    const vertices = [_]Vertex{
-        .{ .position = .{ 0.0, 0.0, 0.0 }, .tex_coord = .{ 0.0, 0.0 } },
-        .{ .position = .{ 1.0, 0.0, 0.0 }, .tex_coord = .{ 1.0, 0.0 } },
-        .{ .position = .{ 1.0, 1.0, 0.0 }, .tex_coord = .{ 1.0, 1.0 } },
-        .{ .position = .{ 0.0, 1.0, 0.0 }, .tex_coord = .{ 0.0, 1.0 } },
-    };
-    const mesh = renderer.createMesh(std.mem.asBytes(&vertices), &[_]u16{ 0, 1, 2, 2, 3, 0 });
-    const chessboard = renderer.addObject(mesh, Mat4.identity(), material);
-
-    var calibrated = false;
-    var tracked_objects = std.AutoHashMap(u64, engine.Renderer.ObjectHandle).init(allocator);
-    defer tracked_objects.deinit();
-
-    while (window.poll()) {
-        const width: f32 = @floatFromInt(window.getWidth());
-        const height: f32 = @floatFromInt(window.getHeight());
-
-        if (calibrated) {
-            renderer.setObjectTransform(chessboard, Mat4.scale(.{ 0, 0, 0 }));
-        } else {
-            renderer.setObjectTransform(chessboard, Mat4.scale(.{ width, height, 1 }));
-        }
-
-        const ui_projection = Mat4.ortho(width, height, -1.0, 1.0);
-        _ = renderer.render(&ui_projection, &ui_projection);
-
-        while (pose_detector.nextEvent()) |event| {
-            const detection = event.detection orelse {
-                const object = tracked_objects.get(event.id).?;
-                renderer.deleteObject(object);
-                if (!tracked_objects.remove(event.id)) {
-                    std.log.err("tracked object wasn't deleted", .{});
-                }
-                continue;
-            };
-
-            const left_hand = detection.keypoints[9].pos;
-            const translate = Mat4.translate(.{ left_hand[0] * width - 25.0, left_hand[1] * height - 25.0, 0 });
-            const scale = Mat4.scale(.{ 50, 50, 1 });
-            const transform = translate.matmul(&scale);
-            const object = renderer.addObject(mesh, transform, material);
-            tracked_objects.put(event.id, object) catch unreachable;
-
-            calibrated = true;
-        }
-    }
+    try runtime.runScript(script_file, script_path);
+    try runtime.run();
 }
