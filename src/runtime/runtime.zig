84c8f7eb54a6a7ad27fe7d4847b55eb1ab4d7c4e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-06-09 20:06:13 -0600

    Add LifetimeBehavior

--- src/runtime/runtime.zig
+++ src/runtime/runtime.zig
@@ -17,15 +17,15 @@ const UpdateEvent = struct {
     delta: f32,
 };
 
-const movement_behavior_events = [_]*const fn (runtime: *Runtime, self: *anyopaque, event: *anyopaque) callconv(.C) void{
-    MovementBehavior.update_handler,
-};
-
 const MovementBehavior = extern struct {
     behavior: Behavior,
     object: *GameObject,
     dx: f32,
     dy: f32,
+
+    const movement_behavior_events = [_]*const fn (runtime: *Runtime, self: *anyopaque, event: *anyopaque) callconv(.C) void{
+        MovementBehavior.update_handler,
+    };
 
     pub fn init(self: *MovementBehavior, object: *GameObject, dx: f32, dy: f32) void {
         self.behavior = .{
@@ -67,6 +67,50 @@ const Behavior = extern struct {
     behavior_instance: *anyopaque,
     num_event_handlers: usize,
     event_handlers: [*]const *const fn (runtime: *Runtime, self: *anyopaque, event: *const anyopaque) callconv(.C) void,
+};
+
+const LifetimeBehavior = struct {
+    behavior: Behavior,
+    object: *GameObject,
+    lifetime: f32,
+    timer: f32,
+
+    const lifetime_behavior_events = [_]*const fn (runtime: *Runtime, self: *anyopaque, event: *anyopaque) callconv(.C) void{
+        LifetimeBehavior.update_handler,
+    };
+
+    pub fn init(self: *LifetimeBehavior, object: *GameObject, lifetime: f32) void {
+        self.behavior = .{
+            .behavior_instance = @ptrCast(self),
+            .num_event_handlers = 1,
+            .event_handlers = @ptrCast(&lifetime_behavior_events),
+        };
+
+        self.object = object;
+        self.lifetime = lifetime;
+        self.timer = 0;
+    }
+
+    pub fn py__init__(user_data: *anyopaque, self_any: engine.Scripting.Any, object_any: engine.Scripting.Any, lifetime: f64) void {
+        const runtime: *Runtime = @alignCast(@ptrCast(user_data));
+        const self = runtime.scripting.getSelf(LifetimeBehavior, self_any) orelse return;
+        const object = runtime.scripting.getSelf(GameObject, object_any) orelse return;
+        runtime.scripting.keepMemberAlive(self_any, object_any, "object");
+        self.init(object, @floatCast(lifetime));
+    }
+
+    pub fn update(runtime: *Runtime, self: *LifetimeBehavior, delta_ms: f32) void {
+        self.timer += delta_ms;
+        if (self.timer >= self.lifetime) {
+            self.object.delete(runtime);
+        }
+    }
+
+    pub fn update_handler(runtime: *Runtime, self_any: *anyopaque, event_any: *const anyopaque) callconv(.C) void {
+        const self: *LifetimeBehavior = @alignCast(@ptrCast(self_any));
+        const event: *const UpdateEvent = @alignCast(@ptrCast(event_any));
+        LifetimeBehavior.update(runtime, self, event.delta);
+    }
 };
 
 const GameObject = struct {
@@ -216,6 +260,9 @@ const Runtime = struct {
 
         try runtime.native_behaviors.append(try runtime.scripting.defineClass(MovementBehavior, module));
         runtime.scripting.defineMethod(MovementBehavior, "__init__", MovementBehavior.py__init__);
+
+        try runtime.native_behaviors.append(try runtime.scripting.defineClass(LifetimeBehavior, module));
+        runtime.scripting.defineMethod(LifetimeBehavior, "__init__", LifetimeBehavior.py__init__);
 
         runtime.objects = std.ArrayList(*GameObject).init(runtime.allocator);
 
