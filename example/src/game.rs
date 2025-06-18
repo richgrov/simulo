200f1b356d7136f3423dccd65fd53b63ca8b20c2
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-06-18 12:52:59 -0400

    Replace python docs with rust setup

---
+++ example/src/game.rs
@@ -0,0 +1,3 @@
+use crate::simulo::*;
+
+pub fn start() {}
--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,19 +1,7 @@
-unsafe extern "C" {
-    fn simulo_create_object(x: f32, y: f32) -> u32;
-    fn simulo_set_object_position(id: u32, x: f32, y: f32);
-    fn simulo_set_object_scale(id: u32, x: f32, y: f32);
-    fn simulo_delete_object(id: u32);
-}
+mod game;
+mod simulo;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn init(root: u32) {
-    unsafe {
-        let object_id = simulo_create_object(50.0, 50.0);
-
-        simulo_set_object_position(object_id, 500.0, 500.0);
-
-        simulo_set_object_scale(object_id, 250.0, 250.0);
-
-        simulo_delete_object(object_id);
-    }
+pub extern "C" fn init(_root: u32) {
+    game::start();
 }
