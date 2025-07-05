e7410d9bed3d231e0b5e900f6371e5bc382c2973
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-04 18:05:51 -0600

    Increase aarch64 assembler capabilities

--- runtime/wasm/asm_aarch64.zig
+++ runtime/wasm/asm_aarch64.zig
@@ -1,5 +1,10 @@
+const std = @import("std");
+
 const Register = enum(u32) {
+    x0,
+    x1,
     lr = 30,
+    sp = 31,
 };
 
 const Assembler = struct {
@@ -12,6 +17,18 @@ const Assembler = struct {
         };
     }
 
+    pub fn mov_reg_to_reg(self: *Assembler, src: Register, dst: Register) void {
+        const base = 0b10101010000000000000001111100000;
+        self.memory[self.write_index] = base | (@intFromEnum(src) << 16) | @intFromEnum(dst);
+        self.write_index += 1;
+    }
+
+    pub fn bl_reg(self: *Assembler, register: Register) void {
+        const base = 0b11010110001111110000000000000000;
+        self.memory[self.write_index] = base | (@intFromEnum(register) << 5);
+        self.write_index += 1;
+    }
+
     pub fn ret_register(self: *Assembler, register: Register) void {
         const base = 0b11010110010111110000000000000000;
         self.memory[self.write_index] = base | (@intFromEnum(register) << 5);
@@ -21,10 +38,45 @@ const Assembler = struct {
     pub fn ret(self: *Assembler) void {
         self.ret_register(Register.lr);
     }
+
+    pub fn strRegisterPreIndex(self: *Assembler, source: Register, offset: i9, into: Register) void {
+        const base = 0b11111000000000000000110000000000;
+        const offsetU: u9 = @bitCast(offset);
+        const offset32: u32 = @intCast(offsetU);
+        self.memory[self.write_index] = base | (offset32 << 12) | (@intFromEnum(into) << 5) | (@intFromEnum(source) << 0);
+        self.write_index += 1;
+    }
+
+    pub fn ldrRegisterPostIndex(self: *Assembler, dest: Register, offset: i9, from: Register) void {
+        const base = 0b11111000010000000000010000000000;
+        const offsetU: u9 = @bitCast(offset);
+        const offset32: u32 = @intCast(offsetU);
+        self.memory[self.write_index] = base | (offset32 << 12) | (@intFromEnum(from) << 5) | (@intFromEnum(dest) << 0);
+        self.write_index += 1;
+    }
+
+    pub fn brk(self: *Assembler) void {
+        self.memory[self.write_index] = 0b11010100001000000000000000000000;
+        self.write_index += 1;
+    }
+
+    pub fn push(self: *Assembler, register: Register) void {
+        self.strRegisterPreIndex(register, -64, Register.sp);
+    }
+
+    pub fn pop(self: *Assembler, register: Register) void {
+        self.ldrRegisterPostIndex(register, 64, Register.sp);
+    }
 };
 
 pub fn writeAssembly(target: *anyopaque) void {
     const assembly: [*c]u32 = @alignCast(@ptrCast(target));
     var assembler = Assembler.init(assembly);
+    assembler.push(Register.lr);
+    assembler.push(Register.x1);
+    assembler.bl_reg(Register.x0);
+    assembler.pop(Register.x1);
+    assembler.pop(Register.lr);
+    assembler.mov_reg_to_reg(Register.x1, Register.x0);
     assembler.ret();
 }
--- runtime/wasm/jit.zig
+++ runtime/wasm/jit.zig
@@ -3,11 +3,15 @@ const std = @import("std");
 const allocation = @import("allocate.zig");
 const assembly = @import("asm.zig");
 
+fn foo() callconv(.C) void {
+    std.debug.print("Hello, world!\n", .{});
+}
+
 pub fn jit() void {
     const buffer = allocation.allocateExecutable(64) catch unreachable;
     assembly.writeAssembly(buffer);
     allocation.finishAllocation(buffer, 64);
-    const func: *const fn () callconv(.C) u64 = @alignCast(@ptrCast(buffer));
-    const result = func();
+    const func: *const fn (x: *const fn () callconv(.C) void, y: u64) callconv(.C) u64 = @alignCast(@ptrCast(buffer));
+    const result = func(foo, 69);
     std.debug.print("{d}\n", .{result});
 }
