7abafb708677d8cb78f23451ddc0b928ae0b13a8
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-07 11:07:13 -0600

    Update library code to use glam

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,19 +1,129 @@
-#[allow(unused)]
-unsafe extern "C" {
-    fn simulo_set_pose_buffer(data: *mut f32);
-    fn simulo_create_object(x: f32, y: f32, material: u32) -> u32;
-    fn simulo_set_object_position(id: u32, x: f32, y: f32);
-    fn simulo_set_object_scale(id: u32, x: f32, y: f32);
-    fn simulo_get_object_x(id: u32) -> f32;
-    fn simulo_get_object_y(id: u32) -> f32;
-    fn simulo_delete_object(id: u32);
-    fn simulo_random() -> f32;
-    fn simulo_window_width() -> i32;
-    fn simulo_window_height() -> i32;
-    fn simulo_create_material(r: f32, g: f32, b: f32) -> u32;
+pub struct GameObject(u32);
+
+#[allow(dead_code)]
+impl GameObject {
+    pub fn new(position: glam::Vec2, material: &Material) -> Self {
+        let id = unsafe { simulo_create_object(position.x, position.y, material.0) };
+        GameObject(id)
+    }
+
+    pub fn position(&self) -> glam::Vec2 {
+        unsafe { glam::Vec2::new(simulo_get_object_x(self.0), simulo_get_object_y(self.0)) }
+    }
+
+    pub fn set_position(&self, pos: glam::Vec2) {
+        unsafe {
+            simulo_set_object_position(self.0, pos.x, pos.y);
+        }
+    }
+
+    pub fn set_scale(&self, scale: glam::Vec2) {
+        unsafe {
+            simulo_set_object_scale(self.0, scale.x, scale.y);
+        }
+    }
+
+    pub fn delete(&self) {
+        unsafe {
+            simulo_delete_object(self.0);
+        }
+    }
 }
 
-static mut GAME: *mut Game = std::ptr::null_mut();
+pub struct Material(u32);
+
+impl Material {
+    pub fn new(r: f32, g: f32, b: f32) -> Self {
+        unsafe { Material(simulo_create_material(r, g, b)) }
+    }
+}
+
+pub fn random_float() -> f32 {
+    unsafe { simulo_random() }
+}
+
+pub fn window_size() -> glam::IVec2 {
+    unsafe { glam::IVec2::new(simulo_window_width(), simulo_window_height()) }
+}
+
+#[derive(Clone)]
+pub struct Pose(pub PoseData);
+
+impl Pose {
+    pub fn nose(&self) -> glam::Vec2 {
+        self.keypoint(0)
+    }
+
+    pub fn left_eye(&self) -> glam::Vec2 {
+        self.keypoint(1)
+    }
+
+    pub fn right_eye(&self) -> glam::Vec2 {
+        self.keypoint(2)
+    }
+
+    pub fn left_ear(&self) -> glam::Vec2 {
+        self.keypoint(3)
+    }
+
+    pub fn right_ear(&self) -> glam::Vec2 {
+        self.keypoint(4)
+    }
+
+    pub fn left_shoulder(&self) -> glam::Vec2 {
+        self.keypoint(5)
+    }
+
+    pub fn right_shoulder(&self) -> glam::Vec2 {
+        self.keypoint(6)
+    }
+
+    pub fn left_elbow(&self) -> glam::Vec2 {
+        self.keypoint(7)
+    }
+
+    pub fn right_elbow(&self) -> glam::Vec2 {
+        self.keypoint(8)
+    }
+
+    pub fn left_wrist(&self) -> glam::Vec2 {
+        self.keypoint(9)
+    }
+
+    pub fn right_wrist(&self) -> glam::Vec2 {
+        self.keypoint(10)
+    }
+
+    pub fn left_hip(&self) -> glam::Vec2 {
+        self.keypoint(11)
+    }
+
+    pub fn right_hip(&self) -> glam::Vec2 {
+        self.keypoint(12)
+    }
+
+    pub fn left_knee(&self) -> glam::Vec2 {
+        self.keypoint(13)
+    }
+
+    pub fn right_knee(&self) -> glam::Vec2 {
+        self.keypoint(14)
+    }
+
+    pub fn left_ankle(&self) -> glam::Vec2 {
+        self.keypoint(15)
+    }
+
+    pub fn right_ankle(&self) -> glam::Vec2 {
+        self.keypoint(16)
+    }
+
+    fn keypoint(&self, index: usize) -> glam::Vec2 {
+        glam::Vec2::new(self.0[index * 2], self.0[index * 2 + 1])
+    }
+}
+
+static mut GAME: *mut crate::game::Game = std::ptr::null_mut();
 
 type PoseData = [f32; 17 * 2];
 static mut POSE_DATA: PoseData = [0.0; 17 * 2];
@@ -21,7 +131,7 @@ static mut POSE_DATA: PoseData = [0.0; 17 * 2];
 #[unsafe(no_mangle)]
 #[allow(static_mut_refs)]
 pub extern "C" fn init(_root: u32) {
-    let g = Box::new(Game::new());
+    let g = Box::new(crate::game::Game::new());
     unsafe {
         GAME = Box::leak(g);
         simulo_set_pose_buffer(POSE_DATA.as_mut_ptr());
@@ -46,82 +156,49 @@ pub extern "C" fn pose(id: u32, alive: bool) {
     }
 }
 
-pub struct GameObject(u32);
-
-#[allow(dead_code)]
-impl GameObject {
-    pub fn new(x: f32, y: f32, material: u32) -> Self {
-        let id = unsafe { simulo_create_object(x, y, material) };
-        GameObject(id)
-    }
-
-    pub fn set_position(&self, x: f32, y: f32) {
-        unsafe {
-            simulo_set_object_position(self.0, x, y);
-        }
-    }
-
-    pub fn x(&self) -> f32 {
-        unsafe { simulo_get_object_x(self.0) }
-    }
-
-    pub fn y(&self) -> f32 {
-        unsafe { simulo_get_object_y(self.0) }
-    }
-
-    pub fn set_scale(&self, x: f32, y: f32) {
-        unsafe {
-            simulo_set_object_scale(self.0, x, y);
-        }
-    }
-
-    pub fn delete(&self) {
-        unsafe {
-            simulo_delete_object(self.0);
-        }
-    }
-}
-
-#[derive(Clone)]
-pub struct Pose(PoseData);
-
-impl Pose {
-    pub fn left_wrist(&self) -> glam::Vec2 {
-        self.keypoint(9)
-    }
-
-    pub fn right_wrist(&self) -> glam::Vec2 {
-        self.keypoint(10)
-    }
-
-    fn keypoint(&self, index: usize) -> glam::Vec2 {
-        glam::Vec2::new(self.0[index * 2], self.0[index * 2 + 1])
-    }
+unsafe extern "C" {
+    fn simulo_set_pose_buffer(data: *mut f32);
+    fn simulo_create_object(x: f32, y: f32, material: u32) -> u32;
+    fn simulo_set_object_position(id: u32, x: f32, y: f32);
+    fn simulo_set_object_scale(id: u32, x: f32, y: f32);
+    fn simulo_get_object_x(id: u32) -> f32;
+    fn simulo_get_object_y(id: u32) -> f32;
+    fn simulo_delete_object(id: u32);
+    fn simulo_random() -> f32;
+    fn simulo_window_width() -> i32;
+    fn simulo_window_height() -> i32;
+    fn simulo_create_material(r: f32, g: f32, b: f32) -> u32;
 }
 
 /////////
 
-pub struct Game {
-    obj: GameObject,
-}
+mod game {
+    use super::*;
+    use glam::Vec2;
 
-impl Game {
-    pub fn new() -> Self {
-        let mat = unsafe { simulo_create_material(1.0, 0.5, 0.25) };
-        let obj = GameObject::new(500.0, 500.0, mat);
-        obj.set_scale(100.0, 100.0);
-        Game { obj }
+    pub struct Game {
+        obj: GameObject,
     }
 
-    pub fn update(&mut self, delta: f32) {
-        self.obj
-            .set_position(self.obj.x() + 50.0 * delta, self.obj.y());
-    }
-
-    pub fn on_pose_update(&mut self, id: u32, pose: Option<&Pose>) {
-        /*if x == -1.0 && y == -1.0 {
-            return;
+    impl Game {
+        pub fn new() -> Self {
+            let mat = Material::new(1.0, 1.0, 1.0);
+            let obj = GameObject::new(Vec2::new(500.0, 500.0), &mat);
+            obj.set_scale(Vec2::new(100.0, 100.0));
+            Game { obj }
         }
-        self.obj.set_position(x, y);*/
+
+        pub fn update(&mut self, delta: f32) {
+            let pos = self.obj.position();
+            let dpos = Vec2::new(50.0 * delta, 0.0);
+            self.obj.set_position(pos + dpos);
+        }
+
+        pub fn on_pose_update(&mut self, id: u32, pose: Option<&Pose>) {
+            /*if x == -1.0 && y == -1.0 {
+                return;
+            }
+            self.obj.set_position(x, y);*/
+        }
     }
 }
