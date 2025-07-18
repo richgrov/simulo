984898ec4bc597b1a6b9e5cd964fb538d77298f5
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-18 09:45:29 -0600

    Add more aarch64 assembler instructions

--- runtime/wasm/asm_aarch64.zig
+++ runtime/wasm/asm_aarch64.zig
@@ -6,8 +6,28 @@ const Module = @import("deserializer.zig").Module;
 const Register = enum(u32) {
     x0,
     x1,
+    x2,
     lr = 30,
     sp = 31,
+};
+
+const Condition = enum(u32) {
+    eq,
+    ne,
+    cs,
+    cc,
+    mi,
+    pl,
+    vs,
+    vc,
+    hi,
+    ls,
+    ge,
+    lt,
+    gt,
+    le,
+    al,
+    nv,
 };
 
 const Assembler = struct {
@@ -23,6 +43,31 @@ const Assembler = struct {
     pub fn mov_reg_to_reg(self: *Assembler, src: Register, dst: Register) void {
         const base = 0b10101010000000000000001111100000;
         self.memory[self.write_index] = base | (@intFromEnum(src) << 16) | @intFromEnum(dst);
+        self.write_index += 1;
+    }
+
+    pub fn sub_imm32(self: *Assembler, src: Register, dest: Register, imm: u12) void {
+        const base = 0b01010001000000000000000000000000;
+        self.memory[self.write_index] = base | (@as(u32, @intCast(imm)) << 10) | (@intFromEnum(src) << 5) | @intFromEnum(dest);
+        self.write_index += 1;
+    }
+
+    pub fn cmp_reg_to_imm32(self: *Assembler, src: Register, imm: u12) void {
+        const base = 0b11110001000000000000000000011111;
+        self.memory[self.write_index] = base | (@as(u32, @intCast(imm)) << 10) | (@intFromEnum(src) << 5);
+        self.write_index += 1;
+    }
+
+    pub fn b(self: *Assembler, offset: i26) void {
+        const offset_u: u26 = @bitCast(offset);
+        const base = 0b00010100000000000000000000000000;
+        self.memory[self.write_index] = base | @as(u32, @intCast(offset_u));
+        self.write_index += 1;
+    }
+
+    pub fn b_cond(self: *Assembler, cond: Condition, offset: i19) void {
+        const base = 0b01010100000000000000000000000000;
+        self.memory[self.write_index] = base | (@as(u32, @intCast(offset)) << 5) | (@intFromEnum(cond) << 0);
         self.write_index += 1;
     }
 
@@ -90,11 +135,22 @@ pub fn writeAssembly(target: *anyopaque, module: *const Module) !?Error {
 
     const assembly: [*c]u32 = @alignCast(@ptrCast(target));
     var assembler = Assembler.init(assembly);
+
+    assembler.cmp_reg_to_imm32(Register.x1, 0);
+    assembler.b_cond(Condition.eq, 12);
+
     assembler.push(Register.lr);
+    assembler.push(Register.x0);
     assembler.push(Register.x1);
-    assembler.bl_reg(Register.x0);
+    assembler.mov_reg_to_reg(Register.x0, Register.x2);
+    assembler.mov_reg_to_reg(Register.x1, Register.x0);
+    assembler.bl_reg(Register.x2);
     assembler.pop(Register.x1);
+    assembler.pop(Register.x0);
     assembler.pop(Register.lr);
+    assembler.sub_imm32(Register.x1, Register.x1, 1);
+    assembler.b(-12);
+
     assembler.mov_reg_to_reg(Register.x1, Register.x0);
     assembler.ret();
     return null;
