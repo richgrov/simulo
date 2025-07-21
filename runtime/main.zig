34054c92a6b82053737f0ab01ad3a8c615779497
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-21 13:46:04 -0600

    Replace cli args with env/.simulo dir

--- runtime/main.zig
+++ runtime/main.zig
@@ -2,15 +2,22 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 const engine = @import("engine");
+const util = @import("util");
 const Runtime = @import("runtime.zig").Runtime;
 
-fn printUsage() void {
-    std.log.err("Usage: simulo <private_key_path> <machine_id> <program_url>", .{});
-}
+fn readPrivateKey(allocator: std.mem.Allocator) ![32]u8 {
+    const home_dir = try std.process.getEnvVarOwned(allocator, "HOME");
+    defer allocator.free(home_dir);
 
-fn readPrivateKey(path: []const u8) ![32]u8 {
+    // From the macOS and Linux user name length limit, 512 bytes should be enough
+    var private_key_path_buf: [512]u8 = undefined;
+    const private_key_path = switch (comptime builtin.target.os.tag) {
+        .macos, .linux => std.fmt.bufPrint(&private_key_path_buf, "{s}/.simulo/private.der", .{home_dir}) catch unreachable,
+        else => @compileError("unsupported platform"),
+    };
+
     var private_key_buf: [68]u8 = undefined;
-    const private_key_der = try std.fs.cwd().readFile(path, &private_key_buf);
+    const private_key_der = try std.fs.cwd().readFile(private_key_path, &private_key_buf);
     if (private_key_der.len < 48) {
         return error.PrivateKeyTooShort;
     }
@@ -29,24 +36,28 @@ pub fn main() !void {
     }
     const allocator = dba.allocator();
 
-    var args = std.process.args();
-    defer args.deinit();
-
-    _ = args.next(); // skip program name
-
-    const private_key_path = args.next() orelse {
-        printUsage();
-        return;
-    };
-    const private_key = readPrivateKey(private_key_path) catch |err| {
-        std.log.err("failed to read private key: {any}", .{err});
+    const private_key = readPrivateKey(allocator) catch |err| {
+        if (err == error.OutOfMemory) {
+            util.crash.oom(error.OutOfMemory);
+        }
+        std.log.err("error: failed to read private key: {s}", .{@errorName(err)});
         return;
     };
 
-    const machine_id = args.next() orelse {
-        printUsage();
-        return;
+    const machine_id = std.process.getEnvVarOwned(allocator, "SIMULO_MACHINE_ID") catch |err| {
+        switch (err) {
+            error.EnvironmentVariableNotFound => {
+                std.log.err("error: SIMULO_MACHINE_ID is not set", .{});
+                return;
+            },
+            error.OutOfMemory => util.crash.oom(error.OutOfMemory),
+            else => {
+                std.log.err("error: failed to get SIMULO_MACHINE_ID: {any}", .{err});
+                return;
+            },
+        }
     };
+    defer allocator.free(machine_id);
 
     try Runtime.globalInit();
     defer Runtime.globalDeinit();
