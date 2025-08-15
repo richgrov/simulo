0ff541de2234332812e31d3f07d7ce362d3a7d91
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-08-14 23:43:58 -0600

    Individual object scripting (#65)
    * Initial commit
    
    * Rename rust APis
    
    * Silence rust style warning
    
    * Fix compile error & leak

---
+++ example/simulo-declare-type/src/lib.rs
@@ -0,0 +1,48 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn ObjectClass(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as DeriveInput);
+    let struct_name = input.ident.clone();
+    
+    let name_string = struct_name.to_string();
+    let hash = generate_hash(&name_string);
+    let hash_str = hash.to_string();
+    
+    let output = quote! {
+        #input
+        
+        impl crate::TypeIdentifiable for #struct_name {
+            const TYPE_ID: u32 = #hash;
+        }
+        
+        paste::paste! {
+            #[unsafe(no_mangle)]
+            pub extern "C" fn [< __vupdate_ #hash_str >](this: *mut std::ffi::c_void, delta: f32) {
+                let ptr = this as *mut #struct_name;
+                let obj = unsafe { &mut *ptr };
+                obj.update(delta);
+            }
+
+            #[unsafe(no_mangle)]
+            pub extern "C" fn [< __vdrop_ #hash_str >](this: *mut std::ffi::c_void) {
+                let ptr = this as *mut #struct_name;
+                unsafe { drop(std::boxed::Box::from_raw(ptr)); }
+            }
+        }
+    };
+    
+    output.into()
+}
+
+fn generate_hash(s: &str) -> u32 {
+    let mut hash: u32 = 0x811c9dc5; // FNV-1a offset basis
+    for byte in s.bytes() {
+        hash ^= byte as u32;
+        hash = hash.wrapping_mul(0x01000193); // FNV-1a prime
+    }
+    hash
+}
--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,10 +1,48 @@
-pub struct GameObject(u32);
+use std::ffi::c_void;
+use simulo_declare_type::ObjectClass;
+
+pub trait TypeIdentifiable {
+    const TYPE_ID: u32;
+}
+
+pub struct BaseObject(u32);
+
+pub trait Object {
+    fn base(&self) -> &BaseObject;
+    fn update(&mut self, _delta: f32) {}
+    fn delete(&mut self) {}
+
+    fn new<T: Object + TypeIdentifiable + 'static>(position: glam::Vec2, material: &Material, f: impl FnOnce(BaseObject) -> T) -> Box<T> {
+        let id = unsafe { simulo_create_object(position.x, position.y, material.0) };
+        let obj = BaseObject(id);
+        let node = f(obj);
+        let b = Box::new(node);
+        let ptr: *const T = &*b;
+
+        let type_id = T::TYPE_ID;
+        unsafe { simulo_set_object_ptr(id, type_id, ptr as *mut T as *mut c_void); }
+        b
+    }
+}
 
 #[allow(dead_code)]
-impl GameObject {
-    pub fn new(position: glam::Vec2, material: &Material) -> Self {
+impl BaseObject {
+    pub fn new<T: Object + TypeIdentifiable + 'static>(position: glam::Vec2, material: &Material, f: impl FnOnce(BaseObject) -> T) -> Box<T> {
         let id = unsafe { simulo_create_object(position.x, position.y, material.0) };
-        GameObject(id)
+        let obj = BaseObject(id);
+        let node = f(obj);
+        let b = Box::new(node);
+        let ptr: *const T = &*b;
+
+        let type_id = T::TYPE_ID;
+        unsafe { simulo_set_object_ptr(id, type_id, ptr as *mut T as *mut c_void); }
+        b
+    }
+
+    pub fn add_child(&self, child: Box<impl Object>) {
+        let child_id = child.base().0;
+        _ = Box::into_raw(child);
+        unsafe { simulo_add_object_child(self.0, child_id); }
     }
 
     pub fn position(&self) -> glam::Vec2 {
@@ -50,8 +88,14 @@ impl GameObject {
 
     pub fn delete(&self) {
         unsafe {
-            simulo_delete_object(self.0);
+            simulo_remove_object(self.0);
         }
+    }
+}
+
+impl std::ops::Drop for BaseObject {
+    fn drop(&mut self) {
+        unsafe { simulo_drop_object(self.0); }
     }
 }
 
@@ -157,18 +201,11 @@ static mut POSE_DATA: PoseData = [0.0; 17 * 2];
 
 #[unsafe(no_mangle)]
 #[allow(static_mut_refs)]
-pub extern "C" fn init(_root: u32) {
-    let g = Box::new(crate::game::Game::new());
+pub extern "C" fn init() {
+    let g = crate::game::Game::new();
     unsafe {
         GAME = Box::leak(g);
         simulo_set_pose_buffer(POSE_DATA.as_mut_ptr());
-    }
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn update(delta: f32) {
-    unsafe {
-        (*GAME).update(delta);
     }
 }
 
@@ -186,6 +223,10 @@ pub extern "C" fn pose(id: u32, alive: bool) {
 unsafe extern "C" {
     fn simulo_set_pose_buffer(data: *mut f32);
     fn simulo_create_object(x: f32, y: f32, material: u32) -> u32;
+    fn simulo_set_object_ptr(id: u32, type_hash: u32, ptr: *mut c_void);
+
+    fn simulo_add_object_child(parent: u32, child: u32);
+
     fn simulo_set_object_position(id: u32, x: f32, y: f32);
     fn simulo_set_object_rotation(id: u32, rotation: f32);
     fn simulo_set_object_scale(id: u32, x: f32, y: f32);
@@ -194,8 +235,10 @@ unsafe extern "C" {
     fn simulo_get_object_rotation(id: u32) -> f32;
     fn simulo_get_object_scale_x(id: u32) -> f32;
     fn simulo_get_object_scale_y(id: u32) -> f32;
+
     fn simulo_set_object_material(id: u32, material: u32);
-    fn simulo_delete_object(id: u32);
+    fn simulo_remove_object(id: u32);
+    fn simulo_drop_object(id: u32);
     fn simulo_random() -> f32;
     fn simulo_window_width() -> i32;
     fn simulo_window_height() -> i32;
@@ -208,34 +251,60 @@ mod game {
     use super::*;
     use glam::Vec2;
 
+    #[ObjectClass]
     pub struct Game {
-        obj: GameObject,
+        base: BaseObject,
+        mat: Material,
     }
 
     impl Game {
-        pub fn new() -> Self {
-            let mat = Material::new(WHITE_PIXEL_IMAGE, 1.0, 1.0, 1.0);
-            let obj = GameObject::new(Vec2::new(500.0, 500.0), &mat);
-            obj.set_scale(Vec2::new(100.0, 100.0));
-            Game { obj }
+        pub fn new() -> Box<Self> {
+            BaseObject::new(Vec2::new(0.0, 0.0), &Material::new(WHITE_PIXEL_IMAGE, 1.0, 1.0, 1.0), |base| {
+                return Game {
+                    base,
+                    mat: Material::new(WHITE_PIXEL_IMAGE, 1.0, 1.0, 1.0),
+                }
+            })
         }
 
-        pub fn update(&mut self, delta: f32) {
-            // let pos = self.obj.position();
-            // let dpos = Vec2::new(50.0 * delta, 0.0);
-            // self.obj.set_position(pos + dpos);
-            // self.obj.set_rotation(self.obj.rotation() + 0.5 * delta);
-
-            let scale = self.obj.get_scale();
-            let dscale = Vec2::new(50.0, 50.0) * delta;
-            self.obj.set_scale(scale + dscale);
-        }
-
-        pub fn on_pose_update(&mut self, id: u32, pose: Option<&Pose>) {
-            /*if x == -1.0 && y == -1.0 {
-                return;
+        pub fn on_pose_update(&mut self, _id: u32, pose: Option<&Pose>) {
+            if let Some(pose) = pose {
+                let particle = BaseObject::new(pose.nose(), &self.mat, |obj| Particle {
+                    base: obj,
+                    lifetime: 1.0,
+                    vel: Vec2::new(50.0, 50.0),
+                });
+                self.base.add_child(particle);
             }
-            self.obj.set_position(x, y);*/
+        }
+    }
+
+    impl Object for Game {
+        fn base(&self) -> &BaseObject {
+            &self.base
+        }
+    }
+
+    #[ObjectClass]
+    struct Particle {
+        base: BaseObject,
+        lifetime: f32,
+        vel: Vec2,
+    }
+
+    impl Object for Particle {
+        fn base(&self) -> &BaseObject {
+            &self.base
+        }
+
+        fn update(&mut self, delta: f32) {
+            let pos = self.base.position();
+            let dpos = self.vel * delta;
+            self.base.set_position(pos + dpos);
+            self.lifetime -= delta;
+            if self.lifetime <= 0.0 {
+                self.base.delete();
+            }
         }
     }
 }
