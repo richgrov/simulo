612834c4d061a48ad775b31be33fda72358003fe
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-08-14 21:33:58 -0600

    Silence rust style warning

--- example/simulo-declare-type/src/lib.rs
+++ example/simulo-declare-type/src/lib.rs
@@ -3,6 +3,7 @@ use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_attribute]
+#[allow(non_snake_case)]
 pub fn ObjectClass(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as DeriveInput);
     let struct_name = input.ident.clone();
