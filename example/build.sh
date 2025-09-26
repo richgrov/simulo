492eadab9f79acc0d215b82a68746785f5b6eb3b
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-09-25 22:39:02 -0600

    Remove old C++ example

--- example/build.sh
+++
@@ -1,5 +0,0 @@
-em++ example/main.cc --no-entry \
-    -sEXPORTED_FUNCTIONS="['_simulo_main', '_simulo__update', '_simulo__recalculate_transform', '_simulo__pose', '_simulo__drop']" \
-    -sSTANDALONE_WASM=1 \
-    -Iexample/glm \
-    -o example/main.wasm
