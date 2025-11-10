46b8919627447d7424528f961abfff74fd3b792e
aog05 <arthur.grover@icloud.com>
2025-11-10 10:46:47 -0700

    Added shader.zig

---
+++ runtime/gpu/vulkan/shader.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const build_options = @import("build_options");
+const vk = @import("vulkan");
+const vkAssert = @import("status.zig").vkAssert;
+
+pub const Shader = struct {
+    device: vk.VkDevice,
+    module: vk.VkShaderModule,
+
+    const Self = @This();
+
+    pub fn init(device: vk.VkDevice, comptime shader_code: []const u8) Self {
+        const create_info = vk.VkShaderModuleCreateInfo{
+            .sType = vk.VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO,
+            .codeSize = shader_code.len,
+            .pCode = shader_code.ptr,
+        };
+        var shader_module: vk.VkShaderModule = null;
+        vkAssert(vk.vkCreateShaderModule(device, &create_info, null, &shader_module)) catch
+            @panic("Failed to create shader module");
+    }
+
+    pub fn deinit(self: *Self) void {
+        vk.vkDestroyShaderModule(self.device, self.module, null);
+    }
+};
