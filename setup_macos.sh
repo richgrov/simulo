04858ac9aaedc192f823d7038d925a3d6a72909b
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-11-17 17:48:30 -0700

    Add build commands for onnxruntime to setup_macos

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,5 +1,10 @@
 set -e
 
+cd vendor/onnxruntime
+./build.sh --config RelWithDebInfo --parallel --compile_no_warning_as_error --skip_submodule_sync --use_vcpkg --cmake_extra_defines CMAKE_OSX_ARCHITECTURES=arm64 --cmake_generator Ninja
+
+cd ../..
+
 mkdir -p vendor/opencv/build
 cd vendor/opencv/build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DWITH_OPENCL=OFF -DWITH_ITT=OFF -DWITH_OPENEXR=OFF -DBUILD_OPENEXR=OFF -G Ninja ..
